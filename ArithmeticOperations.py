4dae73f20e09c30684b44ff4af4ca7a893b4ec3b
HARSHITHA <harshithachava@gmail.com>
2021-04-18 19:31:34 +0530

    ArithmeticOperations

---
+++ ArithmeticOperations.py
@@ -0,0 +1,6 @@
+if __name__ == '__main__':
+    a = int(input())
+    b = int(input())
+    print("The sum of a and b: "+str(a+b))
+    print("The difference between a and b: "+str(abs(a-b)))
+    print("The product of a and b: "+str(a*b))
