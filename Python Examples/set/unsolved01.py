2b238dc11e12c9fa12c0c54c613319faf76a8b43
HARSHITHA <harshithachava@gmail.com>
2021-04-23 00:01:09 +0530

    Set

---
+++ Python Examples/set/unsolved01.py
@@ -0,0 +1,8 @@
+#Write a Python program to find maximum and the minimum value in a set
+set1 = {"a", "b" , "c", "z", "x"}
+print("Maximum of Set1: ",max(set1))
+print("Minimum of Set1: ",min(set1))
+
+set2={1,2,-9,0,23,15}
+print("Maximum of Set2: ",max(set2))
+print("Minimum of Set2: ",min(set2))
